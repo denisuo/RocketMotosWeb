7daec97ae0f5d60efb06516828eb55590040efc6
denisuo <denis_uo@hotmail.com>
2017-05-05 22:29:23 -0300

    primeiro commit

---
+++ src/br/com/rocketmotos/dao/DAO.java
@@ -0,0 +1,10 @@
+package br.com.rocketmotos.dao;
+
+
+public interface DAO {
+
+	public boolean incluir();
+	// TODO: CRIAR Método que receba qualquer classe como parametro para que as
+	// classesDAO das entidades implementem cada um com sua propria classe
+
+}
